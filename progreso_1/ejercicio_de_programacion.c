a36c6eeda47e24d12701fde239e4e4c4f574be1a
Jorge Parra <jorge.parra.chora@udla.edu.ec>
2026-03-29 22:24:15 -0500

    Mi Nuevo Proyecto Desde Cero

---
+++ progreso_1/ejercicio_de_programacion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void);
+int a, b, c;
+printf("Numero inicial: ");
+    scanf("%d", &a);
+
+    printf("Numero final: ");
+    scanf("%d", &b);
+
+    printf("Incremento: ");
+    scanf("%d", &c);
+
+{
+
+
+
+return 0;
+}
