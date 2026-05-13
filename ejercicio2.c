5ce11c1646ef273249c3f4b0022d5ce55c31339a
Jorge Parra <jorgeenriqueparrachora@gmail.com>
2026-05-13 15:02:27 -0500

    tercer

--- ejercicio2.c
+++ ejercicio2.c
@@ -2,14 +2,17 @@
 
 int main () {
 char vocales [] = {'a',' e','i',' o',' u'};
+
 char *puntero;
+
+puntero = &vocales[4];
+*puntero = 'x';
+
 puntero = &vocales[0];
-
-
 for (int i = 0 ; i < 5 ; i ++ ){
 
-    printf ("vocales[%d] %c \n ", i, vocales [i]);
-    printf ("%c", *(puntero++));
+    printf ("vocales[%d] %c  ", i, vocales [i]);
+    printf ("%c\n", *(puntero++));
 
 }
 return 0; 
